49bdc7543b73eb81145726e510ee10027c074d68
venkatsaketh <vvsaketh24@gmail.com>
2024-05-22 22:57:28 +0530

    scroll in home

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import { PiCaretRightBold } from "react-icons/pi";
 import { PiCaretLeftBold } from "react-icons/pi";
 import { useRef } from "react";
+
 const MovieList = ({ movies }) => {
   const movie_card = useRef(null);
   const carousel_ele = useRef(null);
+  const [left, setLeft] = useState(false);
+  const [right, setRight] = useState(true);
+  let scrollEle;
+  const handleScroll = (event) => {
+    if (event.target.scrollLeft > 0) {
+      setLeft(true);
+    } else if (event.target.scrollLeft === 0) setLeft(false);
+
+    if (
+      event.target.scrollLeft >=
+      carousel_ele.current.scrollWidth - carousel_ele.current.offsetWidth - 1
+    )
+      setRight(false);
+    else if (
+      event.target.scrollLeft <
+      carousel_ele.current.scrollWidth - carousel_ele.current.offsetWidth - 1
+    )
+      setRight(true);
+  };
+
+  useEffect(() => {
+    scrollEle = carousel_ele.current;
+    carousel_ele.current.addEventListener("scroll", handleScroll);
+    return () => {
+      scrollEle.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const handleLeft = () => {
     carousel_ele.current.scrollLeft -= 2 * movie_card.current.offsetWidth;
-    console.log(movie_card.current.offsetWidth);
   };
   const handleRight = () => {
     carousel_ele.current.scrollLeft += 2 * movie_card.current.offsetWidth;
@@ -16,12 +44,14 @@ const MovieList = ({ movies }) => {
 
   return (
     <div className="mt-4 ml-3 mr-3 flex c-list relative">
-      <button
-        className="text-white text-3xl mb-1 bg-prev bg-black opacity-70 z-10 w-10 pr-1"
-        onClick={handleLeft}
-      >
-        <PiCaretLeftBold />
-      </button>
+      {left && (
+        <button
+          className={"text-white text-3xl mb-1 bg-prev z-10 w-10 pr-1"}
+          onClick={handleLeft}
+        >
+          <PiCaretLeftBold />
+        </button>
+      )}
       <div
         className="flex overflow-x-scroll carousel scroll-smooth"
         ref={carousel_ele}
@@ -36,12 +66,14 @@ const MovieList = ({ movies }) => {
           />
         ))}
       </div>
-      <button
-        className="text-white text-3xl  my-1 bg-next bg-black opacity-70 w-10 z-10 pl-1"
-        onClick={handleRight}
-      >
-        <PiCaretRightBold />
-      </button>
+      {right && (
+        <button
+          className="text-white text-3xl my-1 bg-next w-10 z-10 pl-1"
+          onClick={handleRight}
+        >
+          <PiCaretRightBold />
+        </button>
+      )}
     </div>
   );
 };
